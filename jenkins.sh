4a2c09280fb33f01fa9579d8b41644fcaf3deb70
root <root@ip-172-31-33-130.ec2.internal>
2024-12-15 16:40:06 +0000

    all files are ready

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+sudo yum install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins
+systemctl status jenkins
+
